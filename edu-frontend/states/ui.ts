39354468ed90b4b09065cc53157ee66ee5742583
jns-w <jns.w@icloud.com>
2025-09-07 22:31:49 +0800

    fix ui state type

--- edu-frontend/states/ui.ts
+++ edu-frontend/states/ui.ts
@@ -1,4 +1,4 @@
 import { atomWithStorage, createJSONStorage } from "jotai/utils"
 import { atom } from "jotai";
 
-export const themeUserPreferenceAtom = atomWithStorage<"dark" | "light">("edu.theme", undefined)
+export const themeUserPreferenceAtom = atomWithStorage<"dark" | "light" | undefined>("edu.theme", undefined)
