2ed220e6074b87f7fdbba2193d7ee62131f89033
jns-w <jns.w@icloud.com>
2025-09-08 00:36:46 +0800

    linting fix

--- edu-frontend/app/page.tsx
+++ edu-frontend/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
                 <div className={styles.welcomeDiv}>
                     <h1>Hi there, welcome to Edu-Fullstack.</h1>
                     <Link href="/courses" className={styles.goToCoursesBtn}>Go to courses</Link>
-                    <p>Or <Link className={styles.signUpLink} href="/auth/signup">sign up</Link> to track progress and access advanced features.</p>
+                    <p>Or <Link href="/auth/signup" className={styles.signUpLink}>sign up</Link> to track progress and access advanced features.</p>
                 </div>
             </main>
         </div>
