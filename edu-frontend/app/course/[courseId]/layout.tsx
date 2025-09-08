c345f5aa7e870b4c4a806f5dc392c732d17282ba
jns-w <jns.w@icloud.com>
2025-09-09 02:51:11 +0800

    styles and dropdown features

--- edu-frontend/app/course/[courseId]/layout.tsx
+++ edu-frontend/app/course/[courseId]/layout.tsx
@@ -1,8 +1,11 @@
 "use client"
+import {FloatingPortal, offset, shift, useFloating} from "@floating-ui/react";
 import {ReactNode, useEffect, useState} from "react";
 import {useParams} from "next/navigation"
+import {List} from "lucide-react";
 import {useAtom} from "jotai";
 
+import {ChapterMenuDropdown} from "@/components/chapter-menu-dropdown/chapter-menu-dropdown";
 import {CourseSearchBar} from "@/components/course-search-bar/course-search-bar";
 import {ChapterMenu} from "@/components/chapter-menu/chapter-menu";
 import {userAtom} from "@/states/user";
@@ -16,14 +19,21 @@ export default function CourseLayout({children}: { children: ReactNode }) {
     const [user] = useAtom(userAtom)
     const [courseTitle, setCourseTitle] = useState("")
     const [chapters, setChapters] = useState([])
-    const content = "## HELLO\nHhaa. Okay man\n\nHere is the thing we are now  \n at a cross roads\n\n```js\ntest\nconst variable = 10\nconst variable2 = [100, 200]\n```\n\n [Bubble sort]() is a sorting algorithm that compares two adjacent elements and swaps them until they are in the intended order.\n\n Just like the movement of air bubbles in the water that rise up to the surface, each element of the array move to the end in each iteration. Therefore, it is called a bubble sort."
-    
+    const [showChaptersDropdown, setShowChaptersDropdown] = useState(false)
+
+    const {floatingStyles, refs} = useFloating({
+        middleware: [offset(3), shift()],
+        onOpenChange: setShowChaptersDropdown,
+        open: showChaptersDropdown,
+        placement: "bottom-end",
+    })
+
     useEffect(() => {
         async function fetchCourseOutline(courseId: number) {
             const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_ENDPOINT}/course/${courseId}/outline`)
             const json = await response.json()
             if (json.ok) {
-                const { data } = json
+                const {data} = json
                 setCourseTitle(data.course.title)
                 setChapters(data.chapters)
             }
@@ -37,12 +47,34 @@ export default function CourseLayout({children}: { children: ReactNode }) {
             <main className={styles.main}>
                 <section className={styles.courseSection}>
                     <div className={styles.courseSectionTopBar}>
-                        {user ? "Progress" : "Sign up to track your progress"}
+                        {user ? "Course Progress" : "Sign up to track your progress"}
                         <CourseSearchBar courseId={courseId}/>
                     </div>
                     <div className={styles.courseContent}>
                         <div className={styles.chapterMenuWrapper}>
-                            <ChapterMenu chapters={chapters} courseTitle={courseTitle} currentCourseId={courseId} currentLessonId={lessonId}/>
+                            <ChapterMenu chapters={chapters} courseTitle={courseTitle} currentCourseId={courseId}
+                                         currentLessonId={lessonId}/>
+                        </div>
+                        <div className={styles.chapterMenuDropdownWrapper}>
+                            <button
+                                ref={refs.setReference}
+                                onClick={() => setShowChaptersDropdown(true)}
+                                className={styles.showChapterMenuDropdownBtn}>
+                                <List size={15}/>
+                                <p>Chapters</p>
+                            </button>
+                            {
+                                showChaptersDropdown && (
+                                    <FloatingPortal>
+                                        <div ref={refs.setFloating} style={floatingStyles}>
+                                        <ChapterMenuDropdown courseTitle={""} chapters={chapters}
+                                                             currentCourseId={courseId} currentLessonId={lessonId}
+                                                             showFn={setShowChaptersDropdown}/>
+                                        </div>
+                                    </FloatingPortal>
+                                )
+                            }
+
                         </div>
                         <div className={styles.articleWrapper}>
                             {children}
--- edu-frontend/components/topbar/user-btn/user-btn.tsx
+++ edu-frontend/components/topbar/user-btn/user-btn.tsx
@@ -1,26 +1,71 @@
 "use client"
-
+import {FloatingPortal, offset, shift, useFloating} from "@floating-ui/react";
+import {RESET} from "jotai/vanilla/utils";
+import {useRouter} from "next/navigation";
+import {User} from "lucide-react";
+import {useState} from "react";
 import {useAtom} from "jotai";
 import Link from "next/link";
 
-import {userAtom} from "@/states/user";
+import {DropdownMenu, MenuItem} from "@/components/dropdown-menu/dropdown-menu";
+import {authTokenAtom, userAtom} from "@/states/user";
 
 import styles from "./user-btn.module.scss"
 
 export function UserBtn() {
-    const [user] = useAtom(userAtom);
+    const [user, setUser] = useAtom(userAtom);
+    const [, setAuthToken] = useAtom(authTokenAtom)
+    const [showDropdown, setShowDropdown] = useState(false)
     
+    const router = useRouter()
+
+    function signOutUser() {
+        setShowDropdown(false)
+        setUser(RESET)
+        setAuthToken(RESET)
+        router.push("/")
+    }
+
+    const {floatingStyles, refs} = useFloating({
+        middleware: [offset(3), shift()],
+        onOpenChange: setShowDropdown,
+        open: showDropdown,
+        placement: "bottom",
+    })
+
+    const dropdownItems: MenuItem[] = [
+        {
+            fn: signOutUser,
+            label: "Sign out",
+        },
+    ]
+
     if (!user) {
         return (
             <div className={styles.signInSignUpDiv}>
-                <Link href={"/auth/signup"} className={styles.getStartedBtn}> Get Started</Link>
+                <Link href={"/auth/signup"} className={styles.getStartedLink}>Get Started</Link>
+                <p>&nbsp;or&nbsp;</p>
+                <Link href={"/auth/signin"} className={styles.signInLink}>Sign In</Link>
             </div>
         )
     }
 
     return (
-        <div>
-            {user?.name}
-        </div>
+        <>
+            <button
+                ref={refs.setReference}
+                className={styles.userBtn}
+                onClick={() => setShowDropdown(true)}>
+                <User size={15} strokeWidth={2}/>
+                <p>{user.name}</p>
+            </button>
+            {showDropdown && (
+                <FloatingPortal>
+                    <div ref={refs.setFloating} style={floatingStyles}>
+                        <DropdownMenu items={dropdownItems} showFn={setShowDropdown}/>
+                    </div>
+                </FloatingPortal>
+            )}
+        </>
     )
 }
