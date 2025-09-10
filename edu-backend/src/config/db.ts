17c4f7c2731804afbb5f43c0d8760b448711fedf
jns-w <jns.w@icloud.com>
2025-09-10 23:53:04 +0800

    fix

--- edu-backend/src/config/db.ts
+++ edu-backend/src/config/db.ts
@@ -6,20 +6,20 @@ import dotenv from "dotenv";
 dotenv.config()
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL || "postgresql://msur:SD3Zbdv2YhMGtC3@psql.capybara.wldspace.com/edu_db",
+	connectionString: process.env.DATABASE_URL,
 });
 
 export const db = drizzle(pool, { schema });
 
 export async function connectDB() {
-    try {
-        await pool.connect().then(async (client) => {
-            console.log("connected to postgres", process.env.DATABASE_URL)
-            await client.query("SELECT NOW()");
-            client.release();
-        })
-    } catch (error) {
-        console.error("Failed to connect to postgres", error)
-        process.exit(1);
-    }
+	try {
+		await pool.connect().then(async (client) => {
+			console.log("connected to postgres", process.env.DATABASE_URL)
+			await client.query("SELECT NOW()");
+			client.release();
+		})
+	} catch (error) {
+		console.error("Failed to connect to postgres", error)
+		process.exit(1);
+	}
 }
