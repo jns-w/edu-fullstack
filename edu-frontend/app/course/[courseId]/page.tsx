9f585ea9bad962a0ccb6cb4a1671d9f8e5351ec6
jns-w <jns.w@icloud.com>
2025-09-11 20:54:58 +0800

    code clean up

--- edu-frontend/app/course/[courseId]/page.tsx
+++ edu-frontend/app/course/[courseId]/page.tsx
@@ -12,8 +12,6 @@ export default function CoursePage() {
     const [title, setTitle] = useState("")
     const [content, setContent] = useState("")
 
-    // const content = "## HELLO\nHhaa. Okay man\n\nHere is the thing we are now  \n at a cross roads\n\n```js\ntest\nconst variable = 10\nconst variable2 = [100, 200]\n```\n\n [Bubble sort]() is a sorting algorithm that compares two adjacent elements and swaps them until they are in the intended order.\n\n Just like the movement of air bubbles in the water that rise up to the surface, each element of the array move to the end in each iteration. Therefore, it is called a bubble sort."
-    
     useEffect(() => {
         async function fetchData(courseId: number): Promise<void> {
             const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_ENDPOINT}/course/${courseId}/content`);
