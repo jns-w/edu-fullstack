cbd67dcb0321507fadbe2506d33f51760bc2db47
jns-w <jns.w@icloud.com>
2025-09-12 10:01:47 +0800

    provide bearer token for fetching data when user is logged in

--- edu-frontend/app/course/[courseId]/page.tsx
+++ edu-frontend/app/course/[courseId]/page.tsx
@@ -1,31 +1,46 @@
 "use client"
 import {useParams} from "next/navigation"
 import {useEffect, useState} from "react";
+import {useAtom} from "jotai";
 
 import {MarkdownContent} from "@/components/markdown-content/markdown-content";
+import {authTokenAtom, userAtom} from "@/states/user";
 
 import styles from "./course-page.module.scss"
 
 export default function CoursePage() {
     const params = useParams()
     const courseId = parseInt(params.courseId as string)
+    const [user] = useAtom(userAtom)
+    const [authToken] = useAtom(authTokenAtom)
     const [title, setTitle] = useState("")
     const [content, setContent] = useState("")
 
     useEffect(() => {
         async function fetchData(courseId: number): Promise<void> {
-            const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_ENDPOINT}/course/${courseId}/content`);
+            const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_ENDPOINT}/course/${courseId}/content`,
+                {
+                    method: "GET",
+                    // add bearer token if user is logged in
+                    ...(user && authToken && {
+                        headers: {
+                            Authorization: `Bearer ${authToken}`
+                        }
+                    })
+                }
+            );
             const json = await response.json()
 
             if (json.ok) {
-                const { data } = json
+                const {data} = json
                 setTitle(data.title)
                 setContent(data.content)
             }
 
         }
+
         fetchData(courseId).then()
-    }, [courseId])
+    }, [authToken, courseId, user])
 
     return (
         <article className={styles.article}>
